7951406f77acfea3639bcc831b6d7723e04e1d40
Kolia Kapiernik <kapiernik27@gmail.com>
2021-06-09 19:49:18 +0300

    Added padding

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -11,6 +11,7 @@ class Filter extends Component {
             max-width: 300px;
             background-color: #FFF;
             box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, .1);
+            padding-bottom: 10px;
         `;
 
         const Title = styled.h3`
