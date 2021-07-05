dd96b2bbcab0989a0190750be3e02ffedc60f27d
Kolia Kapiernik <kapiernik27@gmail.com>
2021-07-05 11:47:14 +0300

    Добавил активный пропс

--- src/components/tabs/tabs.jsx
+++ src/components/tabs/tabs.jsx
@@ -3,29 +3,31 @@ import { ButtonGroup } from 'reactstrap';
 import styled from 'styled-components';
 
 const Button = styled.button`
-background-color: #FFF;
-color: #4A4A4A;
-padding: 15px;
-border: 1px solid #b4b4b4;
-font-weight: 600;
-font-size: 12px;
-letter-spacing: 0.5px;
-text-transform: uppercase;
-&:active{
-    background-color: #2196F3 !important;
-    color: #FFF;
-}
-&:hover{
-    background-color: #f1fcff;
-}
-&:first-of-type{
-    border-top-left-radius: 5px;
-    border-bottom-left-radius: 5px;
-}
-&:last-of-type{
-    border-top-right-radius: 5px;
-    border-bottom-right-radius: 5px;
-}
+    background-color: ${props => props.active ? '#2196F3' : '#FFF'};
+    color: ${props => props.active ? '#FFF' : '#4A4A4A'};
+    padding: 15px;
+    border: 1px solid #b4b4b4;
+    font-weight: 600;
+    font-size: 12px;
+    letter-spacing: 0.5px;
+    text-transform: uppercase;
+    user-select: ${props => props.active ? 'none' : 'auto'};
+    cursor: ${props => props.active ? 'default' : 'pointer'};
+    &:active{
+        background-color: #2196F3 !important;
+        color: #FFF;
+    }
+    &:hover{
+        background-color: ${props => props.active ? '#2196F3' : '#f1fcff'};
+    }
+    &:first-of-type{
+        border-top-left-radius: 5px;
+        border-bottom-left-radius: 5px;
+    }
+    &:last-of-type{
+        border-top-right-radius: 5px;
+        border-bottom-right-radius: 5px;
+    }
 `;
 
 class Tabs extends Component {
